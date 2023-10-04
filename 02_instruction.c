f585c75a837ea9b2e3ee4d29c96c7531f0874e59
Abhay Kumar Mittal <123982720+Abhay809@users.noreply.github.com>
2023-10-04 18:14:00 +0530

    Add files via upload

---
+++ 02_instruction.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    // //int a=4; //type declaration instruction
+    // int a=4 ,b,c;//type declaration instruction
+    // //a=b=c; then output is a=0,b=0,c=0
+    // b=c=a;
+    // printf("the value of a is %d\n",a);
+    // printf("the value of b is %d\n",b);
+    // printf("the value of c is %d\n",c);
+
+
+   float a=1.1;
+   float b=a+8.9;
+   printf("the value of b is %f",b); 
+    return 0;
+}
